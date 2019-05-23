f9c56a5039022496599034609fb130e9d3b5ce58
XS170202103179 <1427075105@qq.com>
2019-05-24 01:40:55 +0800

    更改

--- src/demoProj/student1.java
+++ src/demoProj/student1.java
@@ -1,9 +1,6 @@
 package demoProj;
-public class student {
-   public String name;
-   public String address;
-   
-   student(){
-	   
-   }
+			public class jiaowu {
+				public static void main(String[] args) {
+					System.out.println("good branch!");
+				}
 }
